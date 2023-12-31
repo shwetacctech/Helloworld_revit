e8dab453317954c71e006e61532328e79d82e0c6
shwetacctech <149310316+shwetacctech@users.noreply.github.com>
2023-12-31 18:54:27 +0530

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+        
+        private void TODOBtn_Click(object sender, RoutedEventArgs e)
+        {
+           string todotext= todoInput.Text;
+           if(!string.IsNullOrEmpty(todotext) )
+            {
+                TextBlock todoItem = new TextBlock()
+                {
+                    Text = todotext,
+                    FontWeight = FontWeights.Bold
+                };
+                todoList.Children.Add( todoItem );
+
+                todoInput.Clear();
+            }
+        }
+    }
+    
+}
